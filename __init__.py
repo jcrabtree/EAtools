28d7752c358ce512d896228d75c5aca585aff64c
David Hume <djhume@gmail.com>
2013-07-10 09:21:10 +1200

    Added EAstyles dir for plotting and colour styles in iPython notebooks using matplotlib

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from data_warehouse.utilities import *
 from cds_gnash.gnasher import *
-from colours.ea_colours import *
-from colours.participant_colours import *
+from EAstyles.ea_styles import *
+#from colours.participant_colours import *
