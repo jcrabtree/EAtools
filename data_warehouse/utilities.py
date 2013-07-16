d72a02bc15a5fa544494bf3133aeb0a920a0e6c0
Market Performance (NZ) <imm@ea.govt.nz>
2013-07-16 16:50:59 +1200

    Added get_prices

--- data_warehouse/utilities.py
+++ data_warehouse/utilities.py
@@ -79,6 +79,33 @@ def dw_grab(query,DSN,ts_convert = False,keep_tp_index=True,groupby_level=0):
         if groupby_level>0:
             df = df.groupby(level=list(arange(groupby_level))).sum()
     return df
-	
+
+def get_prices(dateBeg,dateEnd,tpBeg,tpEnd):
+    
+    def parsedate(x):
+        return dt.datetime(int(x.split('-')[0]),int(x.split('-')[1]),int(x.split('-')[2]))
+    q=r"""Select 
+        atomic.DIM_DATE_TIME.DIM_CIVIL_DATE as 'Date',
+        atomic.Atm_Spdsolved_Pnodes.period as 'TP',
+        atomic.Atm_Spdsolved_Pnodes.pnode as 'node',
+        atomic.Atm_Spdsolved_Pnodes.price As 'price'
+    From
+        atomic.Atm_Spdsolved_Pnodes Inner Join
+        atomic.DIM_DATE_TIME On atomic.Atm_Spdsolved_Pnodes.DIM_DTTM_ID =
+        atomic.DIM_DATE_TIME.DIM_DATE_TIME_ID
+    Where
+        atomic.DIM_DATE_TIME.DIM_CIVIL_DATE Between '%s' And '%s' And
+        atomic.Atm_Spdsolved_Pnodes.period Between '%s' And '%s' And
+        LEN(atomic.Atm_Spdsolved_Pnodes.pnode) > 3 And
+        LEN(atomic.Atm_Spdsolved_Pnodes.pnode) < 8
+     """ % (dateBeg.strftime("%Y-%m-%d"),dateEnd.strftime("%Y-%m-%d"),tpBeg,tpEnd)
+    t = sql.read_frame(q,conNZX,coerce_float=True) 
+    t['Date'] = t['Date'].map(lambda x: parsedate(x))
+    t = t.set_index(['Date','TP','node']).price
+    t = t.unstack(level=2)
+    
+    return t
+
+
 if __name__ == '__main__':
     pass
