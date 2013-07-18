f5d444852ac363d8243352de86edc680a59a527b
David Hume <djhume@gmail.com>
2013-07-19 09:37:04 +1200

    Updated queries.py, get_prices() to include a GXP/GIP node list, ie, can now call df=ea.get_prices(connection,datetime_beg,datetime_end,tp_beg,tp_end,nodelist=['BEN2201','OTA2201']) to return just the price series at Benmore and Otahuhu

--- data_warehouse/queries.py
+++ data_warehouse/queries.py
@@ -156,36 +156,65 @@ def get_qwop(connection,dateBeg,dateEnd,company):
     return t
 
 
-def get_prices(connection,dateBeg,dateEnd,tpBeg,tpEnd):
+def get_prices(connection,dateBeg,dateEnd,tpBeg,tpEnd,nodelist=None):
     import pandas.io.sql as sql
     import pyodbc
     import datetime as dt
-    
     def parsedate(x):
         return dt.datetime(int(x.split('-')[0]),int(x.split('-')[1]),int(x.split('-')[2]))
-    q=r"""Select 
-        atomic.DIM_DATE_TIME.DIM_CIVIL_DATE as 'Date',
-        atomic.Atm_Spdsolved_Pnodes.period as 'TP',
-        atomic.Atm_Spdsolved_Pnodes.pnode as 'node',
-        atomic.Atm_Spdsolved_Pnodes.price As 'price'
-    From
-        atomic.Atm_Spdsolved_Pnodes Inner Join
-        atomic.DIM_DATE_TIME On atomic.Atm_Spdsolved_Pnodes.DIM_DTTM_ID =
-        atomic.DIM_DATE_TIME.DIM_DATE_TIME_ID
-    Where
-        atomic.DIM_DATE_TIME.DIM_CIVIL_DATE Between '%s' And '%s' And
-        atomic.Atm_Spdsolved_Pnodes.period Between '%s' And '%s' And
-        LEN(atomic.Atm_Spdsolved_Pnodes.pnode) > 3 And
-        LEN(atomic.Atm_Spdsolved_Pnodes.pnode) < 8
-     """ % (dateBeg.strftime("%Y-%m-%d"),dateEnd.strftime("%Y-%m-%d"),tpBeg,tpEnd)
-    t = sql.read_frame(q,connection,coerce_float=True) 
-    t['Date'] = t['Date'].map(lambda x: parsedate(x))
-    t = t.set_index(['Date','TP','node']).price
-    t = t.unstack(level=2)
+    if nodelist:
+        t = {}
+        for node in nodelist:
+            print "getting %s from DW" % node
+            q=r"""Select 
+                  atomic.DIM_DATE_TIME.DIM_CIVIL_DATE as 'Date',
+                  atomic.Atm_Spdsolved_Pnodes.period as 'TP',
+                  atomic.Atm_Spdsolved_Pnodes.DATA_DTTM as 'DateTime',
+                  atomic.Atm_Spdsolved_Pnodes.price As '%s'
+              From
+                  atomic.Atm_Spdsolved_Pnodes Inner Join
+                  atomic.DIM_DATE_TIME On atomic.Atm_Spdsolved_Pnodes.DIM_DTTM_ID =
+                  atomic.DIM_DATE_TIME.DIM_DATE_TIME_ID
+              Where
+                  atomic.DIM_DATE_TIME.DIM_CIVIL_DATE Between '%s' And '%s' And
+                  atomic.Atm_Spdsolved_Pnodes.period Between '%s' And '%s' And
+                  atomic.Atm_Spdsolved_Pnodes.pnode = '%s'
+              Order by
+                  [Date],[TP]  Asc""" % (node,dateBeg.strftime("%Y-%m-%d"),dateEnd.strftime("%Y-%m-%d"),tpBeg,tpEnd,node)
+    
+            #Read the query 
+            s = sql.read_frame(q,connection,coerce_float=True) 
+            t['Date'] = s['Date']
+            t['TP']= s['TP']       
+            t[node]=s[node]
+
+        t=DataFrame(t)
+        t['Date'] = t['Date'].map(lambda x: parsedate(x))
+        t = t.set_index(['Date','TP'])
+
+    else:
+        q=r"""Select 
+              atomic.DIM_DATE_TIME.DIM_CIVIL_DATE as 'Date',
+              atomic.Atm_Spdsolved_Pnodes.period as 'TP',
+              atomic.Atm_Spdsolved_Pnodes.pnode as 'node',
+              atomic.Atm_Spdsolved_Pnodes.price As 'price'
+          From
+              atomic.Atm_Spdsolved_Pnodes Inner Join
+              atomic.DIM_DATE_TIME On atomic.Atm_Spdsolved_Pnodes.DIM_DTTM_ID =
+              atomic.DIM_DATE_TIME.DIM_DATE_TIME_ID
+          Where
+              atomic.DIM_DATE_TIME.DIM_CIVIL_DATE Between '%s' And '%s' And
+              atomic.Atm_Spdsolved_Pnodes.period Between '%s' And '%s' And
+              LEN(atomic.Atm_Spdsolved_Pnodes.pnode) > 3 And
+              LEN(atomic.Atm_Spdsolved_Pnodes.pnode) < 8""" % (dateBeg.strftime("%Y-%m-%d"),dateEnd.strftime("%Y-%m-%d"),tpBeg,tpEnd) 
+        t = sql.read_frame(q,connection,coerce_float=True) 
+        t['Date'] = t['Date'].map(lambda x: parsedate(x))
+        t = t.set_index(['Date','TP','node']).price
+        t = t.unstack(level=2)
     
     return t
 
-def get_load(connection,dateBeg,dateEnd,tpBeg,tpEnd):
+def get_load(connection,dateBeg,dateEnd,tpBeg,tpEnd,nodelist=None):
     import pandas.io.sql as sql
     import pyodbc
     import datetime as dt
