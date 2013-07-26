d5c9c41e509183fc8f174490b3f98c9c1b40832d
David Hume <djhume@gmail.com>
2013-07-26 14:49:31 +1200

    Added month dictionaries

--- data_warehouse/utilities.py
+++ data_warehouse/utilities.py
@@ -1,6 +1,9 @@
 from pandas import *
 from datetime import date, datetime, time, timedelta
 
+month1 = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
+month2 = {1:'January',2,'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'}
+
 def set_options():
     set_option('display.expand_frame_repr', True)
     set_option('display.line_width',10000)
