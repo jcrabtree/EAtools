6d1c00d7294725b4f8fee555fedb0eedd3cf7ea2
David Hume <djhume@gmail.com>
2013-07-29 10:21:51 +1200

    Update queries.py

--- data_warehouse/queries.py
+++ data_warehouse/queries.py
@@ -214,13 +214,15 @@ def get_prices(connection,dateBeg,dateEnd,tpBeg,tpEnd,nodelist=None):
     
     return t
 
-def get_load(connection,dateBeg,dateEnd,tpBeg,tpEnd,nodelist=None):
+def get_load(connection,dateBeg,dateEnd,tpBeg,tpEnd,windows=False):
     import pandas.io.sql as sql
     import pyodbc
     import datetime as dt
- 
+     
     def parsedate(x):
+        print x
         return dt.datetime(int(x.split('-')[0]),int(x.split('-')[1]),int(x.split('-')[2]))
+        
     q=r"""Select 
         atomic.DIM_DATE_TIME.DIM_CIVIL_DATE as 'Date',
         atomic.Atm_Spdsolved_Pnodes.period as 'TP',
@@ -238,12 +240,15 @@ def get_load(connection,dateBeg,dateEnd,tpBeg,tpEnd,nodelist=None):
      """ % (dateBeg,dateEnd,tpBeg,tpEnd)
     
     t = sql.read_frame(q,connection,coerce_float=True) 
-    t['Date'] = t['Date'].map(lambda x: parsedate(x))
-    t = t.set_index(['Date','TP','node']).demand
-    t = t.unstack(level=2)
+    
+    if not windows:
+        t['Date'] = t['Date'].map(lambda x: parsedate(x))
+        t = t.set_index(['Date','TP','node']).demand
+        t = t.unstack(level=2)
     
     return t
-    
+
+
 
 
 if __name__ == '__main__':
